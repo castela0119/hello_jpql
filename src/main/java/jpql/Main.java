13d392a0d89f7d19e6954f7c98e9c5c6975a331b
castela0119 <78953029+castela0119@users.noreply.github.com>
2022-12-20 19:46:46 +0900

    10.2 기본 문법과 쿼리 API - getResultList() - 결과가 하나 이상일때

--- src/main/java/jpql/Main.java
+++ src/main/java/jpql/Main.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,8 +18,11 @@ public class Main {
             em.persist(member);
 
             TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
-            Query query3 = em.createQuery("select m.username, m.age from Member m");
+            List<Member> resultList = query1.getResultList();
+
+            for (Member member1 : resultList) {
+                System.out.println("member1 = " + member1);
+            }
 
             tx.commit();
         } catch (Exception e) {
