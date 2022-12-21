d73a690d98ad6f15cdce8bf18ff83ce7b0c840f1
castela0119 <78953029+castela0119@users.noreply.github.com>
2022-12-21 19:38:51 +0900

    10.9 JPQL 함수 - (2)LOCATE

--- src/main/java/jpql/Main.java
+++ src/main/java/jpql/Main.java
@@ -13,28 +13,23 @@ public class Main {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            em.persist(member1);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setType(MemberType.ADMIN);
-
-            member.setTeam(team);
-
-            em.persist(member);
+            Member member2 = new Member();
+            member1.setUsername("관리자2");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            String query = "select nullif(m.username, '관리자') as username from Member m";
+            String query = "select locate('de', 'abcdef') From Member m";
 
-            List<String> result = em.createQuery(query, String.class)
+            List<Integer> result = em.createQuery(query, Integer.class)
                     .getResultList();
 
-            for (String s : result) {
+            for (Integer s : result) {
                 System.out.println("s = " + s);
             }
 
